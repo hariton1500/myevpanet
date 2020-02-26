53384291b1b88b9affbb92cf833033848f787b04
evpadallas <evpadallas@gmail.com>
2020-02-26 13:55:01 +0300

    Design 26.02.2020

---
+++ lib/helpers/DesignHelper.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CircleButton extends StatelessWidget {
+  final GestureTapCallback onTap;
+  final IconData iconData;
+
+  const CircleButton({Key key, this.onTap, this.iconData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double size = 40.0;
+
+    return new InkResponse(
+      onTap: onTap,
+      child: new Container(
+        width: size,
+        height: size,
+        decoration: new BoxDecoration(
+          color: Colors.red,
+          shape: BoxShape.circle,
+        ),
+        child: new Icon(
+          iconData,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+}
