367d0ce663066e864810ad5cf625781a6692207b
hariton1500 <hariton1500@gmail.com>
2020-02-29 23:21:58 +0300

    to new api process

--- lib/login_screen/login_widget.dart
+++ lib/login_screen/login_widget.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:mask_text_input_formatter/mask_text_input_formatter.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:myevpanet/login_screen/order_screen.dart';
 import 'package:myevpanet/main.dart';
 import 'package:myevpanet/main_screen/main_widget.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
@@ -71,7 +72,7 @@ class LoginWidgetState extends State with SingleTickerProviderStateMixin{
   Widget _buildSubmitButton() {
     return
       RaisedButton(
-        onPressed: authButtonPressed,
+        onPressed: devKey != null ? authButtonPressed : null,
         color: Theme.of(context).primaryColorDark,
         child: Padding(
           padding: EdgeInsets.all(16.0),
@@ -79,7 +80,7 @@ class LoginWidgetState extends State with SingleTickerProviderStateMixin{
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
               Text(
-                'Войти в личный кабинет',
+                devKey != null ? 'Войти в личный кабинет' : 'Нет доступа к интернету',
                 style: TextStyle(
                   fontSize: 15,
                   fontWeight: FontWeight.w700,
@@ -176,10 +177,10 @@ class LoginWidgetState extends State with SingleTickerProviderStateMixin{
           )
       ),
       floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
+        onPressed: devKey != null ? () {
           // Тут колбасим открытие новой страницы с возможностью оставить заявку на подключение (Используем новый API).
-          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => MainScreenWidget()));
-        },
+          Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => MakeOrder()));
+        } : null,
         label: Text('Оставить заявку'),
         icon: Icon(Icons.person_add),
         backgroundColor: Theme.of(context).accentColor,
@@ -187,80 +188,68 @@ class LoginWidgetState extends State with SingleTickerProviderStateMixin{
     );
   }
 
-
   void authButtonPressed() async {
-
-    //var _model = ScopedModel.of<MainModel>(context);
     var _phoneNumber = phone.getUnmaskedText();
     var _userID = 0;
     if (uid.getUnmaskedText() != '') {
       _userID = int.parse(uid.getUnmaskedText());
     }
 
-    print(_userID);
-    print(_phoneNumber.length);
-
     if (_phoneNumber.length == 11 && _userID > 0) {
-      //_model.updatePhone(int.parse(_phoneNumber));
-      //_model.updateID(_userID);
+      //номер телефона достаточной длины и ИД тоже корректный можно идти дальше
       var phone = int.parse(_phoneNumber);
       var id = _userID;
-      print('Entered phone number is: $phone');
-      print('Entered ID is: $id');
-      //requesting auth from server
-      var _url = 'https://app.evpanet.com/';
-      _url += '?reg&t=$phone&id=$id';
-      _url += '&devid=$devKey';
+      if (verbose >=1) print('Entered phone number is: $phone');
+      if (verbose >=1) print('Entered ID is: $id');
 
-      var result = await RestAPI().getData(_url);
-      print(result);
+      //requesting auth from server
+      Map<String, String> result = await RestAPI().authorizeUserPOST('+' + phone.toString(), id, devKey);
+
+      //обработка ответа сервера
       bool _checks = true;
-      if (result.toString().contains('00000000-0000-0000-C000-000000000046')) {
-        print('Auth failed. No such users.');
-        Fluttertoast.showToast(
-        msg: "Такой пользователь не найден",
-        toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
-        timeInSecForIos: 2,
-        backgroundColor: Colors.red,
-        textColor: Colors.white,
-        fontSize: 16.0
-        );
+      if (result['answer'] == 'isFull') {
+        var _body = json.decode(result['body']);
+        if (verbose >=1) print(_body);
+        if (!_body['error']) {
+          //ошибок нет. в ответе лежат гуиды
+          guids = _body['message']['guids'];
+        } else {
+          //в ответе есть ошибка и ее значение выводим
+          _checks = false;
+          if (verbose >= 1) print('Got Error: ${_body['message']}');
+          Fluttertoast.showToast(
+            msg: "Абонент(ов) с такими данными не найдено",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0
+          );
+        }
+      } else {
+        //ответ был не получен из-за ошибки сети
         _checks = false;
-      }
-      print(result.toString().length);
-      if (result.toString().length < 38) {
-        print('Got bad answer. Do nothing');
-        _checks = false;
-      }
-      if (result.toString().contains('Exception')) {
-        print('Got Exception Error');
-        _checks = false;
+        if (verbose >=1) print('network error :(');
       }
       if (_checks) {
         setState(() => isLoading = true);
-        print('Got good GUID(s)');
+        if (verbose >=1) print('Got good GUID(s)');
         //fbHelper.saveDeviceToken(result);
-        guids = json.decode(result);
         //save guids to file
-        print(guids);
+        if (verbose >=1) print(guids);
         final _guidsfile = await FileStorage('guidlist.dat').localFile;
         _guidsfile.writeAsString(result.toString(), mode: FileMode.write, encoding: utf8);
         currentGuidIndex = 0;
         for (var guid in guids) {
-          var usersRequest = await UserInfo().getUserInfoFromServer();
-          print('$guid: $usersRequest');
+          var usersRequest = await RestAPI().userDataGet(guid, devKey);
+          if (verbose >=1) print('$guid: $usersRequest');
           currentGuidIndex++;
         }
         currentGuidIndex = 0;
         setState(() => isLoading = false);
         Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MainScreenWidget()));
       }
-      //_url = 'https://evpanet.com/api/apk/login/user';
-      //var newres = await RestAPI().postData(_url, _model.phone, _model.id, int.parse(_model.dev_key));
-      //print(newres);
-      //print('Go to Main Screen');
-      //Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (BuildContext context) => MainScreenWidget()));
     } else {
       Fluttertoast.showToast(
         msg: "Введены некорректные данные",
